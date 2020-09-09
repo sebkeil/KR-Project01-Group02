7068564b9208fdb45d3fac9932c5267a922a83bb
martonovic <70942971+martonovic@users.noreply.github.com>
2020-09-09 14:11:35 +0200

    Add files via upload
    made a simple heuristic, whereby the literals are ranked based on the frequency of their respective polarity count

---
+++ .idea/heuristic.py
@@ -0,0 +1,24 @@
+class heuristic:
+    def __init__(self, lit: int, ratio: float):
+        self.lit = lit
+        self.ratio = ratio
+
+    def __repr__(self):
+        return 'literal = ' + str(self.lit) + ', ratio = ' + str(self.ratio)
+
+# the idea of this heuristic is to rank literals based on the ratio of their polarity count
+def heuristic_polarityRatio(absVarbs, varbs, varbCount): # absVarbs  = list of literals, disregarding polarity
+    assignmentsRanked = []                               # varbs     = list of literals with polarity
+    for varb in absVarbs:                                # varbCount = count of literals wrt. to polarity
+        posVarbCount = varbCount[varbs.index(varb)]
+        negVarbCount = varbCount[varbs.index(-varb)]
+        if posVarbCount >= negVarbCount:
+            varbRatio = posVarbCount / negVarbCount
+            varbPol = varb
+        else:
+            varbRatio = negVarbCount / posVarbCount
+            varbPol = -varb
+
+        assignmentsRanked.append(heuristic(varbPol, varbRatio))
+
+    return assignmentsRanked
