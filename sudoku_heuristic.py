853fb6022a25600e67db8a79fe10858a067ae382
martonovic <70942971+martonovic@users.noreply.github.com>
2020-09-16 13:41:19 +0200

    Update sudoku_heuristic.py

--- sudoku_heuristic.py
+++ sudoku_heuristic.py
@@ -1,6 +1,6 @@
-# another sudoku heuristic which I assume works quite well. Because any assignments will always elimate some units from
-# some clauses, unit literals will always appear. These are then inserted to the top of the list of variables to inspect
-# Because the initial assignments are already given for sudoku, this should work without any backtracking at all
+# another sudoku heuristic which I assume works quite well. Because any assignments will always eliminate some units from some
+# clauses, clauses with unit literals will always appear. These are then inserted to the top of the list of variables to inspect.
+# Because the initial assignments are already given for sudoku, this heuristic should work without any backtracking at all
 def sudoku_heuristic(variables, clauses):
     clauses.sort(key=len)
     n = 0
