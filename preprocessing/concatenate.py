983a8acbf2eed30e09c2f0fd49fe5f025bc2942f
maxiele <70944441+maxiele@users.noreply.github.com>
2020-09-21 13:38:16 +0200

    Update concatenate.py

--- preprocessing/concatenate.py
+++ preprocessing/concatenate.py
@@ -1,17 +1,16 @@
 import glob
 
+# read in all txt files
 files = glob.glob('*.txt')
-print(files)
 
-# make sure first the sudoku rules are read in, then the given sudoku
-if files[1] == 'sudoku-rules.txt':
-    order = [1, 0]
-    files = [files[i] for i in order]
-print(files)
+# find the index of the sudoku rules
+rules_index = files.index('sudoku-rules.txt')
 
-# create new file including both the the sudoku rules and the given sudoku
-with open("sudoku_inputfile.txt", "wb") as outfile:
-    for f in files:
-        with open(f, "rb") as infile:
+# create new sudoku_test (rules + puzzle) for each puzzle
+for index_file, this_file in enumerate(files):
+    with open("sudoku_test{}.txt".format(index_file+1), "wb") as outfile:
+        with open(files[rules_index], "rb") as rules:
+            outfile.write(rules.read())
+        with open(this_file, "rb") as infile: 
             outfile.write(infile.read())
 
