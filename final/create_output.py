4538eba853a712705ebd17d2c9400e261c134398
sebkeil <62847324+sebkeil@users.noreply.github.com>
2020-09-22 12:33:16 +0200

    Update create_output.py

--- final/create_output.py
+++ final/create_output.py
@@ -10,3 +10,16 @@ def create_output(assignments, sudoku_name, difficulty_level):  # pass difficult
     with open(filename, 'w') as f:
         for assignment in assignments:
             f.write('{} 0\n'.format(assignment))
+
+            
+def collect_test_results(assignments, sudoku_name, difficulty_level, initial_assignments, backtrack_counter, units, now_time):
+    filename = 'test_results_file.txt'
+    with open(filename, 'w') as f:
+        f.write('-----------------------------------------------------------')
+        f.write('Difficulty: '.format(difficulty_level))
+        f.write('{}\n'.format(sudoku_name))
+        f.write('Number of initial assignments: {}'.format(len(initial_assignments)))
+        f.write('Number of assignments: {}'.format(len(assignments)))
+        f.write('Number of backtracks: {}'.format(len(backtrack_counter)))
+        f.write('Number of unit literals: {}'.format(len(units)))
+        f.write("--- %s seconds ---\n" % now_time)
